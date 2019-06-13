1020cdc35c3e748845c0dd8337dce80e2f6e106c
007vict <super_vg@bk.ru>
2019-06-13 18:54:01 +0300

    dump db

---
+++ test_project/settings_prod.py
@@ -0,0 +1,13 @@
+DEBUG = False
+ALLOWED_HOSTS = ['*', '134.209.123.102']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db1',
+        'USER': 'django_shop',
+        'PASSWORD': '1888vict',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
