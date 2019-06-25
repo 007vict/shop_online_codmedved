7d953f491760b140ec79510b8778e97366ad3c53
007vict <super_vg@bk.ru>
2019-06-25 15:43:22 +0300

    rename db user

--- test_project/settings_prod1.py
+++ test_project/settings_prod1.py
@@ -6,7 +6,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db1',
         'USER': 'django_shop',
-        'PASSWORD': '1888vict',
+        'PASSWORD': 'django_shop',
         'HOST': 'localhost',
         'PORT': '',
     }
